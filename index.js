3ce5c62718ca257d9f75ca97cd71ef21de5d5f49
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-09-30 08:28:45 +0300

    using loops

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        let message;
+        switch (names[i]) {
+            case "Guadalupe":
+                message = "Thank you, Guadalupe, for the wonderful surprise gift!";
+                break;
+            case "Ollie":
+                message = "Thank you, Ollie, for the wonderful surprise gift!";
+                break;
+            case "Aki":
+                message = "Thank you, Aki, for the wonderful surprise gift!";
+                break;
+            default:
+                message = `Thank you, ${names[i]}, for your support!`;
+        }
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+
+countDown(5);
